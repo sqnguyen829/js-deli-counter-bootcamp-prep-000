a53613309952f859e7a3f618cd471394838761f7
steven nguyen <sqnguyen829@gmail.com>
2019-06-19 19:19:41 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+
+var takeANumber = function(c_Line,c_name){
+  
+  //put a name the user input to the end of the array
+  c_Line.push(c_name);
+   return "Welcome, "+c_name+ ". You are number " + c_Line.length + " in line." 
+  }
+  
+var nowServing = function(c_Line){
+  if (c_Line.length > 0){
+    //c_Line.shift() using 'shift()' removes the element from the beginning of the array
+    return "Currently serving " + c_Line.shift() + "."
+  }
+  if (c_Line.length === 0){
+    return  "There is nobody waiting to be served!"
+  }
+}
+
+var currentLine = function(line){
+  
+  if (line.length > 0){
+    let j=0;
+    let s=""
+    for (let i = 1; i <= line.length ; i++){
+      if(i < line.length){
+        s +=  i +". "+ line[j++] + ", "
+      }
+      if(i === line.length){
+        s += i + ". " + line[j++]
+      }
+    }
+    return "The line is currently: " + s
+  }
+  
+  if (line.length === 0){
+    return "The line is currently empty."
+  }
+}
+  
+  
